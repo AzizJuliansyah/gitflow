0668fe67aa50e75da5eeb8541a207b4d74d00c54
AzizJuliansyah <123914220+AzizJuliansyah@users.noreply.github.com>
2025-06-21 20:30:57 +0700

    feat(mtk): add feature pengurangan dan pembagian (#2)
    * feature(pengurangan): add feature pengurangan
    
    * feature(pembagian): add feature pembagian
    
    * feature(mtk): add feature pengurangan dan pembagian

--- main.go
+++ main.go
@@ -8,23 +8,8 @@ func sayHello() {
 func greet(nama string) {
     fmt.Println("Halo", nama)
 }
-func tambah(a int, b int) int {
-    return a + b
-}
-func bagi(a int, b int) (int, int) {
-    return a / b, a % b
-}
-
-
 
 func main() {
     sayHello()
     greet("Aziz")
-
-    hasil := tambah(3, 5)
-    fmt.Println("Hasil tambah:", hasil)
-
-    hasilBagi, sisa := bagi(10, 3)
-    fmt.Println("Hasil bagi:", hasilBagi)
-    fmt.Println("Sisa bagi:", sisa)
 }
